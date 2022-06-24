b1b31c1dace7df99bb596ea6e9aabc21e68c8b9a
YaroslavKabay <yaroslav.kabay@gmail.com>
2022-06-24 04:26:44 +0300

    hw22

--- src/components/mission/Mission.js
+++ src/components/mission/Mission.js
@@ -1,7 +1,8 @@
 export default function Mission({item}){
 
     return (
-        <div> {item.mission_name} {item.launch_year} {item.links.mission_patch_small}
+        <div> {item.mission_name} {item.launch_year}
+            <img src={item.links.mission_patch_small}/>
 
 
 
