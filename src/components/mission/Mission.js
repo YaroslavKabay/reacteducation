367b0eca91118492a5360e18e40c4a773d11e8f2
YaroslavKabay <yaroslav.kabay@gmail.com>
2022-06-24 03:30:37 +0300

    hw22

---
+++ src/components/mission/Mission.js
@@ -0,0 +1,14 @@
+xport default function User({item,choseUser}){
+
+    return (
+        <div> {item.id} {item.name}
+
+            <button onClick={() => {
+
+                choseUser(item);
+
+            }}> Check email </button>
+        </div>
+    );
+
+}
