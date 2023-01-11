f27b3b8237541f93aff831cd58fccba279fc9e0d
YaroslavKabay <yaroslav.kabay@gmail.com>
2023-01-11 13:25:54 +0200

    education

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import { setupStore } from './redux/store';
+
+import './index.css';
+import {App} from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const store = setupStore()
+
+root.render(
+    <Provider store={store}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    </Provider>
+);
+
